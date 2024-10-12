9290122f397a39110b1782745cfa0aebc9e7fb22
Levi <158499287+levi178u@users.noreply.github.com>
2024-10-12 17:03:35 +0530

    [2406] [Time Beats: 95.27%] [Memory Beats: 68.69%] - LeetPush

--- 2406-Divide-Intervals-Into-Minimum-Number-of-Groups.cpp
+++ 2406-Divide-Intervals-Into-Minimum-Number-of-Groups.cpp
@@ -4,28 +4,37 @@ typedef pair<int ,int> p;
     int minGroups(vector<vector<int>>& a) {
        // intervals[i] = [lefti, righti] 
        int n= a.size();
-        vector<p> vp;
+        vector<int > v1,v2;
         
 
         for(int i=0 ;i<n ;i++){
             int si= a[i][0];
             int ei= a[i][1];
 
-            vp.push_back({si,1});
-            vp.push_back({ei+1,-1});
+            v1.push_back(si);
+            v2.push_back(ei);
+            
         }
-        sort(vp.begin() ,vp.end());
+        sort(v1.begin() ,v1.end());
+        sort(v2.begin() ,v2.end());
 
         int c=0,maxi =0;
+        int i=0,j=0;
+        while(i<n and j<n){
+            if(v1[i] <= v2[j]){
+                i++;
+                c++;
+            }else{
+                c--;
+                j++;
+            }
+            maxi= max(maxi, c);
 
-        for(auto &i : vp){
-            int x = i.second;
-
-            c +=x;
-
-            maxi=max(maxi, c);
 
         }
+
+
+        
             return maxi;
     }
 };
